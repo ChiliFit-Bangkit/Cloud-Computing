0c2ced769dd69ca507e43494ad9f223f99ccf3ba
Saud Destama Panjaitan <108171888+saudpanjaitan@users.noreply.github.com>
2024-06-16 13:04:51 +0700

    Delete backend/src directory

--- backend/src/services/loadModel.js
+++
@@ -1,7 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-// const tflite = require('@tensorflow/tfjs-tflite')
-
-async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL_JS);
-}
-module.exports = loadModel;
